e7e1a24433c2745f68b4d0700eb1dc8884038ba6
zzy7517 <zz7517@proton.me>
2024-10-12 23:34:42 +0800

    switch to py

--- backend/http.go
+++
@@ -1,7 +0,0 @@
-package backend
-
-import "github.com/gin-gonic/gin"
-
-func HandleError(c *gin.Context, statusCode int, message string, err error) {
-	c.JSON(statusCode, gin.H{"error": message})
-}
--- backend/tts/tts.go
+++
@@ -1,7 +0,0 @@
-package tts
-
-import "github.com/gin-gonic/gin"
-
-func GenerateAudioFiles(c *gin.Context) {
-	byEdgeTTS()
-}
